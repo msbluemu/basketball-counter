146da7f58499365cc0fabbbd913967d269601a8c
msbluemu <124859009+msbluemu@users.noreply.github.com>
2023-02-14 12:28:15 +1100

    commit initial files

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeEl = document.getElementById("home-el")
+let countHome = 0
+
+function homeAdd1() {
+    countHome = countHome + 1
+    homeEl.innerText = countHome
+}
+
+function homeAdd2() {
+    countHome = countHome + 2
+    homeEl.innerText = countHome
+}
+
+function homeAdd3() {
+    countHome = countHome + 3
+    homeEl.innerText = countHome
+}
+
+let guestEl = document.getElementById("guest-el")
+let countGuest = 0
+
+function guestAdd1() {
+    countGuest = countGuest + 1
+    guestEl.innerText = countGuest
+}
+
+function guestAdd2() {
+    countGuest = countGuest + 2
+    guestEl.innerText = countGuest
+}
+
+function guestAdd3() {
+    countGuest = countGuest + 3
+    guestEl.innerText = countGuest
+}
